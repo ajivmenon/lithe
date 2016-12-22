a669901c1bbc6e27158b184fc3439eef649f75c1
ajivmenon <ajivmenon@live.com>
2016-12-22 10:03:15 +0800

    main root

---
+++ LiteratureJournals/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LiteratureJournals.Startup))]
+namespace LiteratureJournals
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
